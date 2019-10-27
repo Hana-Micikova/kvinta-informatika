ccf1b4bd038453bd76bb126aa5f154e227c16ae2
Hana-Micikova <56343490+Hana-Micikova@users.noreply.github.com>
2019-10-27 14:33:13 +0100

    Update luka.py
    zmena šírky trávy na náhodnú (z intervalu 1 až 4)

--- luka.py
+++ luka.py
@@ -1,6 +1,7 @@
 import tkinter
 import random
-canvas=tkinter.Canvas(width=1000,height=1000,bg="lime")
+a,b=1000,1000
+canvas=tkinter.Canvas(width=a,height=b,bg="lime")
 canvas.pack()
 
 def strom(x,y,r):
@@ -8,11 +9,11 @@ def strom(x,y,r):
     canvas.create_oval(x-r,y-r,x+r,y+r,fill="green")
 
 for i in range(1,11):
-    strom(random.randint(0,1000),random.randint(0,1000),random.randint(30,180))
+    strom(random.randint(0,a),random.randint(0,b),random.randint(30,180))
 
 def stebla(x,y):
     for i in range(3,random.randint(4,21)):
-        canvas.create_line(x,y,x+random.randint(-20,20),y-random.randint(0,30),fill="green")
+        canvas.create_line(x,y,x+random.randint(-20,20),y-random.randint(0,30),fill="green",width=random.randint(1,4))
 
 for i in range(0,20):
-    stebla(random.randint(0,1000),random.randint(0,1000))
+    stebla(random.randint(0,a),random.randint(0,b)
