a3961cb2f9e9d5bd8a372a4fa112aeec228b7bf1
Hana-Micikova <56343490+Hana-Micikova@users.noreply.github.com>
2019-11-10 10:44:31 +0100

    Update stvorce23.10.2019.py

--- stvorce23.10.2019.py
+++ stvorce23.10.2019.py
@@ -1,18 +1,18 @@
 import tkinter
 import random
 
-X_MAS,Y_MAS= 1800,1600
+X_MAX,Y_MAX= 1200,900
 
-canvas=tkinter.Canvas(width=X_MAS,height=Y_MAS,bg="white")
+canvas=tkinter.Canvas(width=X_MAX,height=Y_MAX,bg="white")
 canvas.pack()
 
 def stvorce(x,y,r,m,p):
-    for i in range (1,p):
+    for _ in range (1,p):
         canvas.create_rectangle(x-r,y-r,x+r,y+r)
         r=r-m
 
-x=random.randint(0,800)
-y=random.randint(0,600)
-r=random.randint(1,400)
-
-stvorce(random.randint(0+x+r,X_MAS-x-r),random.randint(0+y+r,X_MAS-y-r),r,random.randint(5,15),random.randint(3,15))        
+x=random.randint(0,150)
+y=random.randint(0,150)
+r=random.randint(75,150)
+for _ in range(10):
+      stvorce(random.randint(0+x+r,X_MAX-x-r),random.randint(0+y+r,Y_MAX-y-r),r,random.randint(5,15),random.randint(6,11))        
